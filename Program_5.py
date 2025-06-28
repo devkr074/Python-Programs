fc97bfb93924f911e771a2b98acb338c8e35e0cc
Dev Kumar <devkr074@gmail.com>
2025-06-28 10:05:34 +0530

    Multiple Values Multiple Variables in Python

--- Program_5.py
+++ Program_5.py
@@ -1,6 +1,10 @@
-# Multiple Values in Python
+# Multiple Values Multiple Variables in Python
 
 x,y,z="Orange","Banana","Cherry"
+a=b=c="Mango"
 print(x)
 print(y)
 print(z)
+print(a)
+print(b)
+print(c)
