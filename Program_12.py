8663a0a620daa5e806887eec7ebdcc170547fa8c
Dev Kumar <devkr074@gmail.com>
2025-06-30 10:12:04 +0530

    Modify String in Python

---
+++ Program_12.py
@@ -0,0 +1,11 @@
+# Modify String in Python
+
+a="hello world"
+print(a.upper())
+b="HELLO WORLD"
+print(a.lower())
+c=" Hello World "
+print(c.strip())
+d="Hello World"
+print(d.replace("H","J"))
+print(d.split(" "))
