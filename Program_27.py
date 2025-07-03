99a10116a1e5fa5efedf1e4f97b565eace577035
Dev Kumar <devkr074@gmail.com>
2025-07-03 14:15:35 +0530

    Remove Items from List in Python

---
+++ Program_27.py
@@ -0,0 +1,13 @@
+# Remove Items from List in Python
+
+myList=["Apple","Banana","Cherry","Orange","Kiwi"]
+myList.remove("Banana")
+print(myList)
+myList.pop(2)
+print(myList)
+myList.pop()
+print(myList)
+del myList[0]
+print(myList)
+myList.clear()
+print(myList)
