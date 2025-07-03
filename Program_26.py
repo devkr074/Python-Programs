06493ad57b771d0938c86fe586c1cc9e5654848e
Dev Kumar <devkr074@gmail.com>
2025-07-03 14:08:49 +0530

    Adding Items in List in Python

---
+++ Program_26.py
@@ -0,0 +1,8 @@
+# Adding Items in List in Python
+
+myList=["Apple","Banana","Cherry"]
+myList.append("Orange")
+print(myList)
+myList2=["Mango","Pineapple","Papaya"]
+myList.extend(myList2)
+print(myList)
