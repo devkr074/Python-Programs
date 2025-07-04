ebee0dd55706c2fd6c2372eed70c8975fbde0f6f
Dev Kumar <devkr074@gmail.com>
2025-07-04 10:40:42 +0530

    Sort List in Python

---
+++ Program_30.py
@@ -0,0 +1,8 @@
+# Sort List in Python
+
+myList=["Orange","Mango","Kiwi","Pineapple","Banana"]
+myList.sort()
+print(myList)
+myList=["Orange","Mango","Kiwi","Pineapple","Banana"]
+myList.sort(reverse = True)
+print(myList)
