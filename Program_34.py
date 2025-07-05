b7c0b62f258421c5a75eca5034d44f50836149af
Dev Kumar <devkr074@gmail.com>
2025-07-05 10:54:14 +0530

    Access Tuple Items in Python

---
+++ Program_34.py
@@ -0,0 +1,10 @@
+# Access Tuple Items in Python
+
+myTuple=("Apple","Banana","Cherry","Apple")
+print(myTuple[1])
+print(myTuple[-1])
+myTuple=("Apple","Banana","Cherry","Orange","Kiwi","Melon","Mango")
+print(myTuple[2:5])
+print(myTuple[:4])
+print(myTuple[2:])
+print(myTuple[-4:-1])
