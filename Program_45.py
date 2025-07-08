6cbb422113fcf5497dca41d8a9132780a5804708
Dev Kumar <devkr074@gmail.com>
2025-07-08 22:34:19 +0530

    Dictionaries in Python

---
+++ Program_45.py
@@ -0,0 +1,14 @@
+# Dictionaries in Python
+
+myDict={
+  "brand":"Ford",
+  "model":"Mustang",
+  "year":1964,
+  "year":2020
+}
+print(myDict)
+print(myDict["brand"])
+print(len(myDict))
+print(type(myDict))
+myDict=dict(name="John",age=36,country="Norway")
+print(myDict)
