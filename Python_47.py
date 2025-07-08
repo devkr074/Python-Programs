5b37a448caca5638fa83f723673720a94bd6760c
Dev Kumar <devkr074@gmail.com>
2025-07-08 22:44:48 +0530

    Change Dictionary Items in Python

---
+++ Python_47.py
@@ -0,0 +1,11 @@
+# Change Dictionary Items in Python
+
+myDict={
+  "brand":"Ford",
+  "model":"Mustang",
+  "year":1964
+}
+myDict["year"]=2018
+print(myDict)
+myDict.update({"year":2020})
+print(myDict)
