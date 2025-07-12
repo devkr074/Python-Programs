d8f5a1ed936c336067c6ba83cda06342ed279056
Dev Kumar <devkr074@gmail.com>
2025-07-12 19:37:29 +0530

    Class and Object in Python

---
+++ Program_61.py
@@ -0,0 +1,17 @@
+# Class and Object in Python
+
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def __str__(self):
+        return f"{self.name}({self.age})"
+    def myfunc(self):
+        print("Hello my name is "+self.name)
+p1=Person("John",36)
+print(p1.name)
+print(p1.age)
+p1=Person("John",36)
+print(p1)
+p1=Person("John",36)
+p1.myfunc()
