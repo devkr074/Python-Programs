6d892167ebbcbbb3df65cd588429604143d4f4c1
Dev Kumar <devkr074@gmail.com>
2025-07-30 10:11:19 +0530

    Pandas Cleaning Empty Cells in Python

---
+++ Program_126.py
@@ -0,0 +1,17 @@
+# Pandas Cleaning Empty Cells in Python
+
+import pandas as pd
+
+df = pd.read_csv("data.csv")
+new_df = df.dropna()
+print(new_df.to_string())
+df.dropna(inplace=True)
+print(df.to_string())
+df.fillna(130, inplace=True)
+df.fillna({"Calories": 130}, inplace=True)
+x = df["Calories"].mean()
+df.fillna({"Calories": x}, inplace=True)
+x = df["Calories"].median()
+df.fillna({"Calories": x}, inplace=True)
+x = df["Calories"].mode()[0]
+df.fillna({"Calories": x}, inplace=True)
