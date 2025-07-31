df0cc93c87d9ffa779f1e9749e7ef639a8115888
Dev Kumar <devkr074@gmail.com>
2025-07-31 10:05:16 +0530

    Pandas Plotting in Python

---
+++ Program_131.py
@@ -0,0 +1,14 @@
+# Pandas Plotting in Python
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data.csv")
+df.plot()
+plt.show()
+df.plot(kind="scatter", x="Duration", y="Calories")
+plt.show()
+df.plot(kind="scatter", x="Duration", y="Maxpulse")
+plt.show()
+df["Duration"].plot(kind="hist")
+plt.show()
