71edec4bd3de363e80e3c0458827dfb29e1380cc
Dev Kumar <devkr074@gmail.com>
2025-08-12 14:56:47 +0530

    Matplotlib Pyplot in Python

---
+++ Program_142.py
@@ -0,0 +1,9 @@
+# Matplotlib Pyplot in Python
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+plt.plot(xpoints, ypoints)
+plt.show()
