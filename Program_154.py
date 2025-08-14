37a729d123f2d2659cb0f16a5063f30f3b354f10
Dev Kumar <devkr074@gmail.com>
2025-08-15 01:12:44 +0530

    Machine Learning - Standard Deviation in Python

---
+++ Program_154.py
@@ -0,0 +1,12 @@
+# Machine Learning - Standard Deviation in Python
+
+import numpy
+
+speed = [86, 87, 88, 86, 87, 85, 86]
+x = numpy.std(speed)
+print(x)
+speed = [32, 111, 138, 28, 59, 77, 97]
+x = numpy.std(speed)
+print(x)
+x = numpy.var(speed)
+print(x)
