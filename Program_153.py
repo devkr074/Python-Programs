ee9796a1ce2ab55fb69a46c60ace3e8badd1464d
Dev Kumar <devkr074@gmail.com>
2025-08-15 01:09:48 +0530

    Machine Learning - Mean Median Mode in Python

---
+++ Program_153.py
@@ -0,0 +1,14 @@
+# Machine Learning - Mean Median Mode in Python
+
+import numpy
+from scipy import stats
+
+speed = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
+x = numpy.mean(speed)
+print(x)
+x = numpy.median(speed)
+print(x)
+x = numpy.median(speed)
+print(x)
+x = stats.mode(speed)
+print(x)
