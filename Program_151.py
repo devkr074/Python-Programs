460b7a5981d50859df3164618082457120292307
Dev Kumar <devkr074@gmail.com>
2025-08-14 10:50:08 +0530

    Matplotlib Histograms in Python

---
+++ Program_151.py
@@ -0,0 +1,10 @@
+# Matplotlib Histograms in Python
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.random.normal(170, 10, 250)
+print(x)
+x = np.random.normal(170, 10, 250)
+plt.hist(x)
+plt.show()
