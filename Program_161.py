f13fc0db4798d31eaa595a5a7dc1fc77be24752f
Dev Kumar <devkr074@gmail.com>
2025-08-17 15:43:17 +0530

    Machine Learning - Multiple Regression in Python

---
+++ Program_161.py
@@ -0,0 +1,15 @@
+# Machine Learning - Multiple Regression in Python
+
+import pandas
+from sklearn import linear_model
+
+df = pandas.read_csv("data.csv")
+X = df[["Weight", "Volume"]]
+y = df["CO2"]
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+predictedCO2 = regr.predict([[2300, 1300]])
+print(predictedCO2)
+print(regr.coef_)
+predictedCO2 = regr.predict([[3300, 1300]])
+print(predictedCO2)
